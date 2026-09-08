4e52325be291ee789ba3aab5fba253ee6624b3bf
Erwan <55313521+ErwanExplorer@users.noreply.github.com>
2026-09-08 16:16:15 +0200

    fix(deploy): configure tanstack start for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: { port: 3000 },
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), tailwindcss(), viteReact()],
+  plugins: [tanstackStart(), nitro(), tailwindcss(), viteReact()],
 })
